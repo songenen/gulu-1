2610a8dec93b2d20d37dbb4d143a70a8153ed78d
Songsong <614750803@qq.com>
2020-09-12 19:10:33 +0800

    添加菊花

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,9 @@
 <template>
   <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
     <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+    <g-icon class="loading" name="loading"></g-icon>
     <div class="content">
-      <slot></slot>
+      <slot/>
     </div>
   </button>
 </template>
@@ -23,6 +24,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
 .g-button {
   font-size: var(--font-size);
   height: var(--button-height);
@@ -67,6 +72,9 @@ export default {
     > .content {
       order: 1;
     }
+  }
+  .loading{
+    animation: spin 2s infinite linear;
   }
 }
 
