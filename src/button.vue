7fe7cab88154ae7f95bc2c3f68b049c4ef0e0a9a
Songsong <614750803@qq.com>
2020-09-12 17:52:22 +0800

    验证iconPosition 为left或right中的一个

--- src/button.vue
+++ src/button.vue
@@ -1,41 +1,75 @@
 <template>
-<button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-  <svg v-if="icon" class="icon">
-    <use  :xlink:href=`#i${icon}`>
-    </use>
-  </svg>
-  <div class="content">
-    <slot></slot>
-  </div>
-</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href=`#i${icon}`>
+      </use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
-<script >
- export default {
-    props:['icon','iconPosition']
+<script>
+export default {
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator(value) {
+        return value === 'left' || value === 'right'
+      }
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-.g-button{
+.g-button {
   font-size: var(--font-size);
   height: var(--button-height);
   padding: 0 1em;
-  border-radius:var(--border-radius);
+  border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
   display: inline-flex;
   justify-content: center;
   align-items: center;
   vertical-align: middle;
-  &:hover{ border-color:var(--border-color-hover)  }
-  &:active{  background-color:var(--button-active-bg);  }
-  &:focus{ outline: none;  }
-  > .content{ order: 2;  }
-  > .icon{ order: 1;margin-left: 0; margin-right: .3em }
-  &.icon-right{
-    > .icon{order: 2;margin-right: 0; margin-left: .3em}
-    > .content{ order: 1;}
+
+  &:hover {
+    border-color: var(--border-color-hover)
+  }
+
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  > .content {
+    order: 2;
+  }
+
+  > .icon {
+    order: 1;
+    margin-left: 0;
+    margin-right: .3em
+  }
+
+  &.icon-right {
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: .3em
+    }
+
+    > .content {
+      order: 1;
+    }
   }
 }
 
